031414bfecaecfa52803a366ddb2d40a76b47065
karmani1997 <mehtab.karmani@gmail.com>
2023-08-24 16:10:42 +0200

    feat: add new features in the API

--- index.js
+++ index.js
@@ -6,6 +6,49 @@ const port = 3000
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
+const requestLoggingMiddleware = (req, res, next) => {
+
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+    console.log('Request Headers:', req.headers);
+    console.log('Request Body:', req.body);
+    next();
+};
+
+
+const responseLoggingMiddleware = (req, res, next) => {
+    const originalSend = res.send;
+    res.send = function(data) {
+      console.log('Response Body:', data);
+      originalSend.call(this, data);
+    };
+    next();
+  };
+
+// Middleware function
+// const errorMiddleware = (err,req, res, next) => {
+//     try {
+//       // Some logic that might throw an error
+//       //const value = someFunctionThatDoesNotExist(); // This will throw an error
+//       console.log('Value***********:', err);
+      
+//       next(); // Move to the next middleware/route handler
+//     } catch (error) {
+//       console.error('An error occurred:', error.message);
+//       res.status(500).send('Internal Server Error');
+//     }
+//   };
+function errorHandler (err, req, res, next) {
+    res.status(500)
+    res.render('error', { error: err })
+  }
+
+app.use(requestLoggingMiddleware); 
+
+app.use(responseLoggingMiddleware); 
+app.use(errorHandler);
+
+
+
 let users = [{
     id: 1,
     name: 'Mehtab',}, 
@@ -30,16 +73,21 @@ app.get('/users', (req, res) => {
 
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
-    console.log({id})
+    //console.log({id})
     const user = users.find((user) => user.id==id);
-    console.log(user)
+    //console.log(user)
     res.send(user);
+})
+
+app.get('/error', (req, res) => {
+    //res.send("Error in backend")
+    throw new Error('An internal error occured')
 })
 
 app.post('/users', (req, res) => {
 
     const user = req.body
-    console.log(user)
+    // console.log(user)
     users.push(user)
     res.send(users)
 })
@@ -50,6 +98,11 @@ app.delete('/users/:id', (req, res) => {
     users = users.filter(user => user.id != id)
     res.send(users)
 })
+
+
+
+  
+
 
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`)
